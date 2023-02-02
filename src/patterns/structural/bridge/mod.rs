c50e876bd39419dc780bec193a26c431ee10948a
Viacheslav <gitcoy@outlook.com>
2023-02-02 18:57:49 +0700

    Change bridge

--- src/patterns/structural/bridge/mod.rs
+++ src/patterns/structural/bridge/mod.rs
@@ -1,5 +1,3 @@
-use crate::patterns::structural::bridge::abstract_chart::AbstractChart;
-
 pub mod abstract_chart;
 pub mod abstract_chart_imp;
 
@@ -8,6 +6,7 @@ pub mod chart_imp;
 
 // Example
 pub fn structural_bridge() {
+    use abstract_chart::AbstractChart;
     use chart::gtk4_drawing_area_chart::Gtk4DrawingAreaChart;
     let mut chart = Gtk4DrawingAreaChart::new();
 
